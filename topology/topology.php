ff5b29d61458c537e9becb4f0987eb32ef88a7f6
Naomi Navkal <navkal@hotmail.com>
2017-05-23 19:11:48 -0400

    Tree map in JSON format

--- topology/topology.php
+++ topology/topology.php
@@ -5,13 +5,18 @@
 <div class="container">
   <div id="headerContent" class="page-header" >
     <div class="row" >
-      <div class="col-xs-12 col-sm-4 col-md-3 col-lg-2 headerButton" >
-        <a class="btn btn-default btn-sm" href="topology/circuitTopology.pdf" target="_blank" title="View diagram of Circuit Topology" >
-          <span class="glyphicon glyphicon-blackboard"></span> View Circuit Topology
+      <div class="col-xs-12 col-sm-3 col-md-2 col-lg-2 headerButton" >
+        <a href="topology/circuitTopology.pdf" target="_blank" title="Open Circuit Diagram in a new page" >
+          <span class="glyphicon glyphicon-blackboard"></span> Circuit Diagram
         </a>
       </div>
-      <div class="col-xs-12 col-sm-8 col-md-9 col-lg-10 headerButton" >
-        <a class="btn btn-default btn-sm" href="topology/downloadTree.php" onclick="return startTreeDump(event);" title="Generate and download dump of Circuit Tree" ><span class="glyphicon glyphicon-download-alt"></span> Download Tree Dump <sup><span class="glyphicon glyphicon-asterisk"></span></sup></a>
+      <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 headerButton" >
+        <a href="topology/tree.html" target="_blank" title="Open Circuit Tree graphic in a new page" >
+          <span class="glyphicon glyphicon-blackboard"></span> Circuit Tree
+        </a>
+      </div>
+      <div class="col-xs-12 col-sm-7 col-md-8 col-lg-8 headerButton" >
+        <a class="btn btn-default btn-sm" href="topology/downloadTree.php" onclick="return startTreeDump(event);" title="Generate and download dump of Circuit Tree" ><span class="glyphicon glyphicon-download-alt"></span> Tree Download <sup><span class="glyphicon glyphicon-asterisk"></span></sup></a>
         <span class="well-sm text-info" style="white-space:nowrap;" >
           <small>
             <sup>
