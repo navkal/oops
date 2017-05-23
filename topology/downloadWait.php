e3bdaf78b123995640a5a2e043afdf376680abf0
Naomi Navkal <navkal@hotmail.com>
2017-05-23 19:27:11 -0400

    Tree map in JSON format

--- topology/downloadWait.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Copyright 2016 Panel Spy.  All rights reserved.
-
-  require_once $_SERVER["DOCUMENT_ROOT"]."/../common/util.php";
-
-  echo( json_encode( $_SESSION["downloadDone"] ) );
-?>
--- topology/topology.php
+++ topology/topology.php
@@ -1,30 +1,17 @@
 <!-- Copyright 2017 Panel Spy.  All rights reserved. -->
 
-<script src="topology/topology.js?version=<?=time()?>"></script>
-
 <div class="container">
   <div id="headerContent" class="page-header" >
     <div class="row" >
-      <div class="col-xs-12 col-sm-3 col-md-2 col-lg-2 headerButton" >
+      <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2" >
         <a href="topology/circuitTopology.pdf" target="_blank" title="Open Circuit Diagram in a new page" >
           <span class="glyphicon glyphicon-blackboard"></span> Circuit Diagram
         </a>
       </div>
-      <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 headerButton" >
+      <div class="col-xs-6 col-sm-2 col-md-2 col-lg-2" >
         <a href="topology/tree.html" target="_blank" title="Open Circuit Tree graphic in a new page" >
           <span class="glyphicon glyphicon-blackboard"></span> Circuit Tree
         </a>
-      </div>
-      <div class="col-xs-12 col-sm-7 col-md-8 col-lg-8 headerButton" >
-        <a class="btn btn-default btn-sm" href="topology/downloadTree.php" onclick="return startTreeDump(event);" title="Generate and download dump of Circuit Tree" ><span class="glyphicon glyphicon-download-alt"></span> Tree Download <sup><span class="glyphicon glyphicon-asterisk"></span></sup></a>
-        <span class="well-sm text-info" style="white-space:nowrap;" >
-          <small>
-            <sup>
-              <span class="glyphicon glyphicon-asterisk"></span>
-            </sup>
-            Takes several minutes
-          </small>
-        </span>
       </div>
     </div>
   </div>
