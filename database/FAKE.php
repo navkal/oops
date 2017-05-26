9f5d5fe1cef130b2e570d958149adae879065892
Naomi Navkal <navkal@hotmail.com>
2017-05-26 01:40:15 -0400

    Topology window

--- database/FAKE.php
+++ database/FAKE.php
@@ -20,17 +20,23 @@
     <a href="javascript:void(null)" path="MSWB" onclick="openImageWindow(event)">MSWB</a>
     <a href="javascript:void(null)" path="MSWB.6-DE" onclick="openImageWindow(event)">MSWB.6-DE</a>
     <a href="javascript:void(null)" path="MSWB.9-AMDP" onclick="openImageWindow(event)">MSWB.9-AMDP</a>
-    <div class='embed-responsive' style='padding-bottom:150%'>
-        <object data='../database/circuitTopology.pdf' type='application/pdf' width='100%' height='100%'></object>
+    <div class="embed-responsive" style="padding-bottom:150%">
+        <object id="pdf" data="../database/circuitTopology.pdf" type="application/pdf" width="100%" ></object>
     </div>
   </body>
 </html>
 
 <script>
   $( window ).on( 'unload', closeChildWindows );
-  
+  $( window ).on( 'resize', resizePdf );
+
   function closeChildWindows()
   {
     childWindowsClose( g_aImageWindows );
+  }
+
+  function resizePdf()
+  {
+    $( '#pdf' ).attr( 'data', '../database/circuitTopology.pdf' );
   }
 </script>
