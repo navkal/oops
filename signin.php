e2de8bb9025dc049c32dfd24683b9d4f86b08b94
Naomi Navkal <navkal@hotmail.com>
2017-06-07 11:56:55 -0400

    Topology window

--- signin.php
+++ signin.php
@@ -1,13 +1,34 @@
 <!-- Copyright 2017 Panel Spy.  All rights reserved. -->
 
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Sign In</title>
-  </head>
+<html>
+  <?php
+    require_once $_SERVER["DOCUMENT_ROOT"]."/../common/head.php";
+  ?>
+
   <body>
-    <form action="/" method="post">
-      <button type="submit">Sign in</button>
-    </form>
+    <div class="container">
+      <div class="page-header">
+        <p class="h3">Panel Spy</p>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                
+          <form action="/" method="post">
+            <div style="text-align:center;" >
+              <button type="submit">Sign in</button>
+            </div>
+          </form>
+
+
+        </div>
+      </div>
+
+    </div>
+
+    <script>
+      document.title = "Sign In - Panel Spy";
+    </script>
+
   </body>
 </html>
