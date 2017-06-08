0959532dac324a2f387d5cb687fd8deb5e8a79f5
Naomi Navkal <navkal@hotmail.com>
2017-06-08 15:32:12 -0400

    Sign-in

--- session/session.js
+++ session/session.js
@@ -31,8 +31,20 @@ function submitCredentials( tEvent )
 
 function signOut()
 {
-  // Close Topology window
-  childWindowsClose( g_aTopologyWindows )
+  // Close Topology child window
+  childWindowsClose( g_aTopologyWindows );
+
+  //
+  // Close Topology and Image windows that might be openers relative to the main window.
+  // This occurs only if the user has closed the main window and then reopened it via the Image window 'back' button.
+  // Note that a Topology window that is 'lost', i.e., no longer reachable via the 'opener' hierarchy, will eventually close itself.
+  //
+  var tOpener = window.opener;
+  while ( tOpener && ( tOpener != window ) && ! tOpener.closed )
+  {
+    tOpener.close();
+    tOpener = tOpener.opener;
+  }
 
   // Post request to server
   var tPostData = new FormData();
