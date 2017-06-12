a6af3b3dbc4e66b3f28d3a3093a87da9d4b7586c
Naomi Navkal <navkal@hotmail.com>
2017-06-12 10:56:56 -0400

    Sign-in

--- session/passwordPrompt.php
+++ session/passwordPrompt.php
@@ -1,15 +1,50 @@
 <!-- Copyright 2017 Panel Spy.  All rights reserved. -->
 
-<?php
-  $_SESSION['panelSpy']['session']['forcePasswordChange'] = false; //////// fake fake fake
-?>
-
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>SORRY</title>
-  </head>
+<html>
+
+  <!-- Head -->
+  <?php
+    require_once $_SERVER["DOCUMENT_ROOT"] . "/../common/head.php";
+  ?>
+
+  <!-- Body -->
   <body>
-    <button onclick="location.assign('/')">CHANGE PASSWORD</button>
+    <div class="container">
+      <div class="page-header">
+        <h3>Update Password</h3>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <form onsubmit="updatePassword(event); return false;" >
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" class="form-control" id="password" placeholder="New Password">
+            </div>
+            <div class="form-group">
+              <label for="password">Confirm</label>
+              <input type="password" class="form-control" id="confirm" placeholder="Confirm New Password">
+            </div>
+            <div style="text-align:center;" >
+              <button id="update" type="submit" onclick="g_sAction='update'" class="btn btn-primary" >Update Password</button>
+              <button id="cancel" type="submit" onclick="g_sAction='cancel'" class="btn btn-default" >Cancel</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <script>
+      document.title = "Update Password - Panel Spy";
+    </script>
+
   </body>
 </html>
+
+<script>
+  function updatePassword( tEvent )
+  {
+    alert( g_sAction );
+
+  }
+</script>
