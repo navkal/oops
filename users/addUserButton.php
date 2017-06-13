59cbc91819f3b955ed73196f5cb0e3e94391b8f5
Naomi Navkal <navkal@hotmail.com>
2017-06-12 22:49:27 -0400

    Sign-in

--- users/addUserButton.php
+++ users/addUserButton.php
@@ -1,5 +1,12 @@
 <!-- Copyright 2017 Panel Spy.  All rights reserved. -->
 
-<button id="addUserButton" class="btn btn-default btn-sm pull-right" onclick="addUser(event)" style="display:none" >
+<button id="addUserButton" class="btn btn-default btn-sm pull-right" onclick="addUser()" style="display:none" >
   <span class="glyphicon glyphicon-plus"></span> Add User
 </button>
+
+<script>
+  function addUser()
+  {
+    alert( 'addUser' );
+  }
+</script>
