26c9cb048243c34e0a1c6a6b67b705bf4ebf0faa
Naomi Navkal <navkal@hotmail.com>
2017-06-16 17:19:51 -0400

    Sign-in

--- database/dbCommon.py
+++ database/dbCommon.py
@@ -1,4 +1,5 @@
 import hashlib
+import time
 
 def hash( text ):
     h = hashlib.md5()
@@ -7,7 +8,8 @@ def hash( text ):
 
 dcEventTypes = {
     'database': 'database',
-    'notes': 'notes'
+    'notes': 'notes',
+    'addUser': 'addUser'
 }
 
 def add_interactive_user( cur, conn, by, username, password, role, description ):
@@ -21,6 +23,10 @@ def add_interactive_user( cur, conn, by, username, password, role, description )
         cur.execute( '''SELECT id FROM Role WHERE role = ?''', (role,))
         role_id = cur.fetchone()[0]
         cur.execute( '''INSERT OR IGNORE INTO User ( username, password, role_id, description, force_change_password ) VALUES (?,?,?,?,? )''', (username, hash(password), role_id, description, True) )
+
+        cur.execute('''INSERT INTO Activity ( timestamp, username, event_type, target_table, target_column, target_value, description )
+            VALUES (?,?,?,?,?,?,? )''', ( time.time(), by, dcEventTypes['addUser'], 'User', 'username', username, "Add user '" + username + "'" ) )
+
         conn.commit()
 
     return bUnique
