74ba684de3a8a841a491898c1c5f797e3687dc11
Naomi Navkal <navkal@hotmail.com>
2017-06-17 19:16:14 -0400

    Clean up debug output

--- users/editUser.php
+++ users/editUser.php
@@ -1,9 +1,8 @@
 <!-- Copyright 2017 Panel Spy.  All rights reserved. -->
 
 <?php
+  require_once $_SERVER["DOCUMENT_ROOT"] . "/users/usernameRules.php";
   require_once $_SERVER["DOCUMENT_ROOT"] . "/users/passwordRules.php";
-  define( 'MAX_USERNAME_LENGTH', 20 );
-  define( 'MIN_USERNAME_LENGTH', 6 );
 ?>
 
 <!-- Add User button -->
@@ -165,44 +164,6 @@
     showMessages( aMessages );
 
     return ( aMessages.length == 0 );
-  }
-
-  function validateUsername()
-  {
-    var tUsername = $( '#username' );
-    var aMessages = [];
-
-    if ( ! tUsername.prop( 'readonly' ) )
-    {
-      var sUsername = tUsername.val();
-
-      if ( sUsername.length > <?=MAX_USERNAME_LENGTH?> )
-      {
-        aMessages.push( 'Username may contain at most <?=MAX_USERNAME_LENGTH?> characters.' );
-      }
-
-      if ( sUsername.length < <?=MIN_USERNAME_LENGTH?> )
-      {
-        aMessages.push( 'Username must contain at least <?=MIN_USERNAME_LENGTH?> characters.' );
-      }
-
-      if ( sUsername.indexOf( ' ' ) != -1 )
-      {
-        aMessages.push( 'Username may not contain spaces.' );
-      }
-
-      if ( ( sUsername.length > 0 ) && ! sUsername.match( /^[a-zA-Z0-9\-_]+$/ ) )
-      {
-        aMessages.push( 'Username can contain only alphanumeric, hyphen, and underscore characters.' );
-      }
-
-      if ( aMessages.length )
-      {
-        tUsername.parent().addClass( 'has-error' );
-      }
-    }
-
-    return aMessages;
   }
 
   function submitUser()
