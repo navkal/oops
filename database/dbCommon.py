59caf4f91c8093eceb24cb2b03adb169891ba85d
Naomi Navkal <navkal@hotmail.com>
2017-06-19 12:34:11 -0400

    Settings

--- database/dbCommon.py
+++ database/dbCommon.py
@@ -14,7 +14,7 @@ dcEventTypes = {
     'removeUser': 'removeUser'
 }
 
-def add_interactive_user( cur, conn, by, username, password, role, description, force_change_password=True ):
+def add_interactive_user( cur, conn, by, username, password, role, description, force_change_password=True, enabled=True, first_name='', last_name='', email_address='', organization='' ):
 
     # Check whether username is unique
     cur.execute( '''SELECT username FROM User WHERE lower(username) = ?''', (username.lower(),))
@@ -24,7 +24,8 @@ def add_interactive_user( cur, conn, by, username, password, role, description, 
     if bUnique:
         cur.execute( '''SELECT id FROM Role WHERE role = ?''', (role,))
         role_id = cur.fetchone()[0]
-        cur.execute( '''INSERT OR IGNORE INTO User ( username, password, role_id, description, force_change_password ) VALUES (?,?,?,?,? )''', (username, hash(password), role_id, description, force_change_password) )
+        cur.execute( '''INSERT OR IGNORE INTO User ( username, password, role_id, description, force_change_password, enabled, first_name, last_name, email_address, organization ) 
+        VALUES (?,?,?,?,?,?,?,?,?,? )''', (username, hash(password), role_id, description, force_change_password, enabled, first_name, last_name, email_address, organization) )
 
         cur.execute('''INSERT INTO Activity ( timestamp, username, event_type, target_table, target_column, target_value, description )
             VALUES (?,?,?,?,?,?,? )''', ( time.time(), by, dcEventTypes['addUser'], 'User', 'username', username, "Add user '" + username + "'" ) )
