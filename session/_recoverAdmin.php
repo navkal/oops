d737fb4de3a609127e412b391da38d2554a05927
Naomi Navkal <navkal@hotmail.com>
2017-06-22 00:40:32 -0400

    Recover admin access

--- session/_recoverAdmin.php
+++ session/_recoverAdmin.php
@@ -7,9 +7,7 @@
   exec( $command, $output, $status );
   error_log( "===> output=" . print_r( $output, true ) );
 
-  // Extract user from database output
-  $sUser = $output[ count( $output ) - 1 ];
-  $_SESSION['panelSpy']['user'] = (array) json_decode( $sUser );
+  $_SESSION['panelSpy']['user'] = [];
 
   // Delete single-use script and redirect
   unlink(__FILE__);
