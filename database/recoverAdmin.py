a3ebf0c3014c0256f939a90fd899a8aa32ce9b8c
Naomi Navkal <navkal@hotmail.com>
2017-06-29 23:00:54 -0400

    Multitenant context

--- database/recoverAdmin.py
+++ database/recoverAdmin.py
@@ -1,9 +1,16 @@
 # Copyright 2017 Panel Spy.  All rights reserved.
 
+import argparse
+import context
 import sqlite3
 import dbCommon
 
-conn = sqlite3.connect('database/database.sqlite')
-cur = conn.cursor()
-cur.execute( 'UPDATE User SET password=?, force_change_password=? WHERE lower(username)=?', ( dbCommon.hash('recoverAdmin'), True, 'admin' ) )
-conn.commit()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser( description='recover admin user' )
+    parser = context.add_context_args( parser )
+    args = parser.parse_args()
+
+    conn = sqlite3.connect('database/' + args.enterprise + '/database.sqlite')
+    cur = conn.cursor()
+    cur.execute( 'UPDATE User SET password=?, force_change_password=? WHERE lower(username)=?', ( dbCommon.hash('recoverAdmin'), True, 'admin' ) )
+    conn.commit()
