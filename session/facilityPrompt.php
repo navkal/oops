043396b6b59b0d70d8cc3d8afb8fe945049103c6
Naomi Navkal <navkal@hotmail.com>
2017-07-05 12:22:40 -0400

    Multitenant context

--- session/facilityPrompt.php
+++ session/facilityPrompt.php
@@ -18,7 +18,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
           <form onsubmit="alert('form'); return false;" >
             <div style="text-align:center;" >
-              <select id="facilityChooser" >
+              <select id="facilityChooser" class="form-control" >
               </select>
               <button type="submit" onclick="alert('submit');" class="btn btn-primary" >Continue</button>
               <button type="button" onclick="alert('quit');" class="btn btn-default" >Quit</button>
@@ -59,16 +59,27 @@
 
   function handleFacilitiesRsp( tRsp, sStatus, tJqXhr )
   {
+    // Extract list of facilities accessible to signed-in user
     var tFacilityMap = tRsp.facility_map;
 
-    var sHtml = '';
-
+    // Format HTML options and save in a map
+    var tHtmlMap = {};
     for ( var sName in tFacilityMap )
     {
       var sDescr = tFacilityMap[sName];
-      sHtml += '<option value="' + sName + '">' + sDescr + '</option>';
+      tHtmlMap[sDescr] = '<option value="' + sName + '">' + sDescr + '</option>';
     }
 
+    // Sort and concatenate options
+    var aKeys = Object.keys( tHtmlMap ).sort();
+    var sHtml = '';
+    for ( var iKey in aKeys )
+    {
+      var sKey = aKeys[iKey];
+      sHtml += tHtmlMap[sKey];
+    }
+
+    // Load options into chooser
     $( '#facilityChooser' ).html( sHtml );
   }
 
