4e9f700d651078ddf8fa35d93277e9a2f6ed7c7d
Naomi Navkal <navkal@hotmail.com>
2017-07-10 23:34:40 -0400

    Multitenant context

--- activity/activity.php
+++ activity/activity.php
@@ -1,7 +1,6 @@
 <!-- Copyright 2017 Panel Spy.  All rights reserved. -->
 
 <?php
-  require_once $_SERVER["DOCUMENT_ROOT"]."/util/security.php";
   require_once $_SERVER["DOCUMENT_ROOT"]."/sortableTable/sortableTable.php";
 ?>
 
