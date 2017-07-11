7e5cb744594917327fef0ccd2b8b2018dddeef3a
Naomi Navkal <navkal@hotmail.com>
2017-07-11 17:37:46 -0400

    Multitenant context

--- topology/topologyLink.php
+++ topology/topologyLink.php
@@ -1,5 +1,9 @@
 <!-- Copyright 2017 Panel Spy.  All rights reserved. -->
 
+<?php
+  require_once $_SERVER["DOCUMENT_ROOT"]."/util/security.php";
+?>
+
 <a id="topologyLink" class="pull-right" href="javascript:void(null)" onclick="openTopologyWindow(event)" target="_blank" title="Open Circuit Topology diagram" style="display:none" >
   <span class="glyphicon glyphicon-blackboard"></span> Topology
 </a>
