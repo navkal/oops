c31be63a9dcd081a30e0ee5bf7e06e932d8a68f9
Naomi Navkal <navkal@hotmail.com>
2017-07-11 17:48:00 -0400

    Multitenant context

--- topology/topology.php
+++ topology/topology.php
@@ -6,7 +6,7 @@
   <!-- Head -->
   <?php
     require_once $_SERVER["DOCUMENT_ROOT"]."/../common/head.php";
-    require_once $_SERVER["DOCUMENT_ROOT"]."/util/abort.php";
+    require_once $_SERVER["DOCUMENT_ROOT"]."/util/security.php";
     if ( ! isset( $_SESSION['panelSpy']['context']['enterprise'], $_SESSION['panelSpy']['context']['facility'] ) )
     {
       abort();
