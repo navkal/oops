f15b393b2f3417220eacef346582613bcbf5034c
Naomi Navkal <navkal@hotmail.com>
2017-07-10 22:42:12 -0400

    Multitenant context

--- contact/contact.php
+++ contact/contact.php
@@ -2,6 +2,7 @@
 
 <div class="container">
   <?php
+    require_once $_SERVER["DOCUMENT_ROOT"]."/util/security.php";
     require_once $_SERVER["DOCUMENT_ROOT"]."/../common/contact.php";
     contact( "h3", "Panel Spy", "We", "The Panel Spy team" );
   ?>
