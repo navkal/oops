51ba791290b25c5c70128c693da6c47e03c74480
Naomi Navkal <navkal@hotmail.com>
2017-07-10 23:20:26 -0400

    Multitenant context

--- devices/devices.php
+++ devices/devices.php
@@ -1,6 +1,7 @@
 <!-- Copyright 2017 Panel Spy.  All rights reserved. -->
 
 <?php
+  require_once $_SERVER["DOCUMENT_ROOT"]."/util/security.php";
   require_once $_SERVER["DOCUMENT_ROOT"]."/sortableTable/sortableTable.php";
 ?>
 
