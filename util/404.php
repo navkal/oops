b4a6fc07f5367dec561c3a2ac1f635aa82ecaa37
Naomi Navkal <navkal@hotmail.com>
2017-07-12 17:20:17 -0400

    Multitenant context

--- util/404.php
+++ util/404.php
@@ -1,9 +1,30 @@
+<!-- Copyright 2017 Panel Spy.  All rights reserved. -->
+
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Error 404</title>
-  </head>
+<html>
+
+  <!-- Head -->
+  <?php
+    require_once $_SERVER["DOCUMENT_ROOT"] . "/../common/head.php";
+  ?>
+
+  <!-- Body -->
   <body>
-    404
+    <div class="container" >
+      <div class="page-header">
+        <img src="../brand.ico" class="img-responsive" alt="Panel Spy" style="width:50%; max-width:250px; margin:auto">
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <div class="alert alert-danger">
+            <b>Requested object not found.</b>
+          </div>
+        </div>
+      </div>
+    </div>
   </body>
-</head>
+</html>
+
+<script>
+  document.title = 'Not Found - Panel Spy';
+</script>
