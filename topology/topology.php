6d8344995533151623efd97f98906bebae4e9eb1
Naomi Navkal <navkal@hotmail.com>
2017-08-16 18:32:26 -0400

    Move topology diagram to menu

--- topology/topology.php
+++ topology/topology.php
@@ -2,27 +2,10 @@
 
 <?php
   require_once $_SERVER["DOCUMENT_ROOT"]."/util/security.php";
+  $iVersion = time();
 ?>
 
-<div class="container-fluid">
-  <div id="zoomButtons" >
-    <span class="btn-group btn-group-xs" >
-      <button class="btn btn-link btn-xs" onclick="zoomDiagram(true)" title="Zoom in">
-        <span class="glyphicon glyphicon-plus" ></span>
-      </button>
-      <br/>
-      <button class="btn btn-link btn-xs" onclick="zoomDiagram(false)" title="Zoom out">
-        <span class="glyphicon glyphicon-minus" ></span>
-      </button>
-    </span>
-  </div>
-  <div class="clearfix" >
-    <?php
-      require_once $_SERVER["DOCUMENT_ROOT"]."/database/" . $_SESSION['panelSpy']['context']['enterprise'] . '/' . $_SESSION['panelSpy']['context']['facility'] . "/topology.svg";
-    ?>
-  </div>
-</div>
-
+<script src="../topology/topology.js?version=<?=$iVersion?>"></script>
 
 <style>
   svg
@@ -48,7 +31,21 @@
   }
 </style>
 
-<?php
-  $iVersion = time();
-?>
-<script src="../topology/topology.js?version=<?=$iVersion?>"></script>
+<div class="container-fluid">
+  <div id="zoomButtons" >
+    <span class="btn-group btn-group-xs" >
+      <button class="btn btn-link btn-xs" onclick="zoomDiagram(true)" title="Zoom in">
+        <span class="glyphicon glyphicon-plus" ></span>
+      </button>
+      <br/>
+      <button class="btn btn-link btn-xs" onclick="zoomDiagram(false)" title="Zoom out">
+        <span class="glyphicon glyphicon-minus" ></span>
+      </button>
+    </span>
+  </div>
+  <div class="clearfix" >
+    <?php
+      require_once $_SERVER["DOCUMENT_ROOT"]."/database/" . $_SESSION['panelSpy']['context']['enterprise'] . '/' . $_SESSION['panelSpy']['context']['facility'] . "/topology.svg";
+    ?>
+  </div>
+</div>
