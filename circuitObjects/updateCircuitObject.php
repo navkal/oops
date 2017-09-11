13528e37ebf58723cb03f16d6b9714c08f59cb25
Naomi Navkal <navkal@hotmail.com>
2017-09-10 22:12:39 -0400

    Add/Update of Circuit Objects - Circuits, Panels, Transformers

--- circuitObjects/updateCircuitObject.php
+++ circuitObjects/updateCircuitObject.php
@@ -29,7 +29,6 @@
   error_log( '==> command=' . $command );
   exec( $command, $output, $status );
   error_log( '==> output=' . print_r( $output, true ) );
-  echo( json_encode( $_POST ) ); exit();
 
   // Echo status
   $sStatus = $output[ count( $output ) - 1 ];
