b18ef2cc44730843a90e402aa89c18e3a523530e
Naomi Navkal <navkal@hotmail.com>
2017-09-21 17:28:04 -0400

    Remove object

---
+++ recycle/recycle.php
@@ -0,0 +1,12 @@
+<!-- Copyright 2017 Panel Spy.  All rights reserved. -->
+
+<?php
+  require_once $_SERVER["DOCUMENT_ROOT"]."/pi/pi.php";
+  //require_once $_SERVER["DOCUMENT_ROOT"]."/sortableTable/sortableTable.php";
+?>
+
+<script>
+  g_sSortableTableTitle = 'Recycle Bin';
+  g_sSortableTableType = 'recycle';
+  $( document ).ready( getSortableTable );
+</script>
--- recycleBin/recycleBin.php
+++
@@ -1,8 +0,0 @@
-<!-- Copyright 2017 Panel Spy.  All rights reserved. -->
-
-<div class="container">
-  <?php
-    require_once $_SERVER["DOCUMENT_ROOT"]."/util/security.php";
-    phpinfo();
-  ?>
-</div>
