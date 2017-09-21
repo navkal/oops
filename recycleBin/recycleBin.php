be6edaa5ab71275b9e10c2e0a7900fcfe5d1b0e8
Naomi Navkal <navkal@hotmail.com>
2017-09-21 17:15:38 -0400

    Remove object

---
+++ recycleBin/recycleBin.php
@@ -0,0 +1,8 @@
+<!-- Copyright 2017 Panel Spy.  All rights reserved. -->
+
+<div class="container">
+  <?php
+    require_once $_SERVER["DOCUMENT_ROOT"]."/util/security.php";
+    phpinfo();
+  ?>
+</div>
