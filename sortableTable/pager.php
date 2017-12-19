e88a92901fbe36cf450ae3098f531e1d0433a48f
Naomi Navkal <navkal@hotmail.com>
2017-12-19 10:21:06 -0500

    Change available page sizes

--- sortableTable/pager.php
+++ sortableTable/pager.php
@@ -56,9 +56,9 @@
             <small>Rows</small>
             <select class="pagesize form-control input-sm">
               <option value="10">10</option>
-              <option value="20">20</option>
-              <option value="30">30</option>
-              <option value="40">40</option>
+              <option value="25">25</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
               <option value="all">All</option>
             </select>
           </span>
