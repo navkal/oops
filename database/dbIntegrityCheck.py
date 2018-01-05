285e8f39267ed7f719fbd7b093a0820900affb95
Naomi Navkal <navkal@hotmail.com>
2018-01-05 16:30:40 -0500

    Refine integrity check

--- database/dbIntegrityCheck.py
+++ database/dbIntegrityCheck.py
@@ -290,15 +290,15 @@ def traverse_paths( cur, subtree, subtree_root_id, panel_type_id, transformer_ty
 
         # Verify that leading portion of kid path matches parent path
         if kid_path_leading != subtree_root_path:
-            messages.append( make_error_message( facility_fullname, dbCommon.get_object_type( cur, kid['object_type_id'] ), kid['path'], 'Path should be child of ' + "'" + subtree_root_path + "'" + '.'  ) )
+            messages.append( make_error_message( facility_fullname, dbCommon.get_object_type( cur, kid['object_type_id'] ), kid['path'], 'Internal database error.  Path should be child of ' + "'" + subtree_root_path + "'" + '.'  ) )
 
         # Verify that kid tail matches trailing element of kid path
         if kid_path_trailing != kid['tail']:
-            messages.append( make_error_message( facility_fullname, dbCommon.get_object_type( cur, kid['object_type_id'] ), kid['path'], 'Tail '  + "'" + kid['tail'] + "'" + ' should match last segment of path.'  ) )
+            messages.append( make_error_message( facility_fullname, dbCommon.get_object_type( cur, kid['object_type_id'] ), kid['path'], 'Internal database error.  Tail '  + "'" + kid['tail'] + "'" + ' should match last segment of path.'  ) )
 
         # Verify that kid source matches parent tail
         if kid['source'] != subtree_root_tail:
-            print( '=========bad tail' )
+            messages.append( make_error_message( facility_fullname, dbCommon.get_object_type( cur, kid['object_type_id'] ), kid['path'], 'Internal database error.  Source '  + "'" + kid['source'] + "'" + ' should match tail of parent ' + "'" + subtree_root_tail + "'" + ' .'  ) )
 
         messages += traverse_paths( cur, subtree, kid_id, panel_type_id, transformer_type_id, facility_fullname )
 
