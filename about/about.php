ef0b168ecb672ed582a70c48023d9e40397c2bc5
Naomi Navkal <navkal@hotmail.com>
2018-01-14 16:44:30 -0500

    Update copyright notice to 2018

--- about/about.php
+++ about/about.php
@@ -5,48 +5,63 @@
 ?>
 
 <div class="container">
-  <h4>About Panel Spy</h4>
-  <hr/>
 
-  <p>
-    The electrical distribution network of a commercial or public building must constantly evolve to meet tenants' changing needs.
-    Electricians who maintain and renovate the network require precise knowledge of its structure to do their work.
-    Typically, they piece that knowledge together from cryptic, hand-written notes found on electrical panel labels; architectural drawings that may be inaccurate or out-of-date; and time-consuming, hands-on testing of electrical connections.
-  </p>
+  <div class="row">
 
-  <p>
-    Panel Spy addresses this problem by providing a dynamic, online model of your building’s electrical distribution network.
-    It allows you to manage your entire inventory of panels, transformers, circuits, and devices, tracking their properties, connectivity, and locations.
-    Most critically, Panel Spy allows you to update all of these features in real time.
-  </p>
+    <div class="col-xs-12 col-sm-8 col-md-8 col-lg-7">
 
-  <p>
-    Panel Spy controls access to the model through user roles that reflect worker responsibility.
-    An electrical intern, for example, could be assigned a role restricted to viewing and navigation, whereas a full-fledged technician could assume a role that allows alteration of the model and recording of contemporaneous notes about work being done.
-  </p>
+      <div style="padding-bottom:18px";>
+        <span class="h4">About Panel Spy</span>
+        <br/>
+      </div>
 
-  <p>
-    You can run Panel Spy in any modern browser, on a computer, smartphone or tablet.
-  </p>
+      <p>
+        The electrical distribution network of a commercial or public building must constantly evolve to meet tenants' changing needs.
+        Electricians who maintain and renovate the network depend on precise knowledge of its structure to do their work.
+        Typically, they piece that knowledge together from cryptic, hand-written notes found on electrical panel labels; architectural drawings that may be inaccurate or out-of-date; and time-consuming, hands-on testing of electrical connections.
+      </p>
 
-  <p>
-    The Panel Spy team is proud to bring this innovative application to the world of building management.
-    Your feedback is important to us.
-    Please use the <a href="/?page=contact">Contact</a> page to convey your suggestions and comments.
-  </p>
+      <p>
+        Panel Spy addresses this problem by providing a dynamic, online model of your building’s electrical distribution network.
+        It allows you to manage your entire inventory of panels, transformers, circuits, and devices, tracking their properties, connectivity, and locations.
+        Most critically, Panel Spy allows you to update all of these features in real time.
+      </p>
 
-  <p>
-    Thank you for using Panel Spy!
-  </p>
+      <p>
+        Panel Spy controls access to the model through user roles that reflect worker responsibility.
+        An electrical intern, for example, could be assigned a role restricted to viewing and navigation, whereas a full-fledged technician could assume a role that allows alteration of the model and recording of contemporaneous notes about work being done.
+      </p>
 
-  <br/>
+      <p>
+        You can run Panel Spy in any modern browser, on a computer, smartphone or tablet.
+      </p>
 
-  <dl class="dl-horizontal">
-    <dt>Anil Navkal</dt>
-    <dd>Lead</dd>
-    <dt>Nikhil Navkal</dt>
-    <dd>Developer, database</dd>
-    <dt>Naomi Navkal</dt>
-    <dd>Developer, user interface</dd>
-  </dl>
+      <p>
+        The Panel Spy team is proud to bring this innovative application to the world of building management.
+        Your feedback is important to us.
+        Please use the <a href="/?page=contact">Contact</a> page to convey your suggestions and comments.
+      </p>
+
+      <p>
+        Thank you for using Panel Spy!
+      </p>
+
+      <br/>
+
+      <dl class="dl-horizontal">
+        <dt>Anil Navkal</dt>
+        <dd>Lead</dd>
+        <dt>Nikhil Navkal</dt>
+        <dd>Developer, database</dd>
+        <dt>Naomi Navkal</dt>
+        <dd>Developer, user interface</dd>
+      </dl>
+
+    </div>
+
+    <div class="hidden-xs col-sm-4 col-md-4 col-lg-5">
+      <img src="about/label.jpg" class="img-responsive" style="max-width:250px; margin:auto;" alt="Panel Label">
+    </div>
+
+  </div>
 </div>
