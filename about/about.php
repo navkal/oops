58241c71da8a7ce8fe87202bf63191850fafb5c5
Naomi Navkal <navkal@hotmail.com>
2018-01-14 14:32:37 -0500

    Update copyright notice to 2018

--- about/about.php
+++ about/about.php
@@ -9,15 +9,20 @@
   <hr/>
 
   <p>
-    The electrical distribution network of a commercial or public building must evolve, through the course of its lifetime, to meet tenants’ changing needs.  The electricians who maintain and renovate the network must know its precise structure.  Yet, that knowledge must constantly be pieced together from cryptic, hand-written notes found on electrical panel labels, architectural drawings that may be inaccurate or out-of-date, and hands-on testing of electrical connections.
+    The electrical distribution network of a commercial or public building must constantly evolve to meet tenants' changing needs.
+    Electricians who maintain and renovate the network require precise knowledge of its structure to do their work.
+    Typically, they piece that knowledge together from cryptic, hand-written notes found on electrical panel labels; architectural drawings that may be inaccurate or out-of-date; and time-consuming, hands-on testing of electrical connections.
   </p>
 
   <p>
-    Panel Spy addresses this problem by providing a dynamic, online model of your building’s electrical distribution network.  It allows you to manage your entire inventory of panels, transformers, circuits, and devices, tracking their properties, connectivity, and locations.   Most critically, Panel Spy allows you to update all of these features in real time.
+    Panel Spy addresses this problem by providing a dynamic, online model of your building’s electrical distribution network.
+    It allows you to manage your entire inventory of panels, transformers, circuits, and devices, tracking their properties, connectivity, and locations.
+    Most critically, Panel Spy allows you to update all of these features in real time.
   </p>
 
   <p>
-    Panel Spy controls access to the model through user roles that reflect worker responsibility.  An electrical intern, for example, could be assigned a role restricted to viewing and navigation, whereas a full-fledged technician could assume a role that allows alteration of the model and recording of contemporaneous notes about work being done.
+    Panel Spy controls access to the model through user roles that reflect worker responsibility.
+    An electrical intern, for example, could be assigned a role restricted to viewing and navigation, whereas a full-fledged technician could assume a role that allows alteration of the model and recording of contemporaneous notes about work being done.
   </p>
 
   <p>
@@ -25,7 +30,9 @@
   </p>
 
   <p>
-    The Panel Spy team is proud to bring this innovative application to the world of building management.  Your feedback is important to us.  Please use the Contact page to convey your suggestions and comments.
+    The Panel Spy team is proud to bring this innovative application to the world of building management.
+    Your feedback is important to us.
+    Please use the Contact page to convey your suggestions and comments.
   </p>
 
   <p>
