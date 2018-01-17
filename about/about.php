9587c03f20e4cb3395402673c85fe837065ab2c1
Naomi Navkal <navkal@hotmail.com>
2018-01-16 23:32:26 -0500

    Source all database content from generator

--- about/about.php
+++ about/about.php
@@ -59,7 +59,7 @@
 
       <div class="hidden-xs col-sm-4 col-md-4 col-lg-5">
         <img id="label" src="about/label.jpg" class="img-responsive" style="max-width:250px; margin:auto;" alt="Panel Label">
-        <img id="hover" src="about/hover.png" class="img-responsive" style="max-width:250px; margin:auto; display:none;" alt="Distribution">
+        <img id="hover" src="about/hover.jpg" class="img-responsive" style="max-width:250px; margin:auto; display:none;" alt="Distribution">
       </div>
 
   </div>
